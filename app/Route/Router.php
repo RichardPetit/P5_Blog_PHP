25d1227ca5de7f023fd3c50b5420cac8822eebb6
Julien Russo <j.russo@magicstay.com>
2021-05-17 18:00:14 +0200

    Update router

--- app/Route/Router.php
+++ app/Route/Router.php
@@ -3,14 +3,16 @@
 
 namespace Blog\Route;
 
+use function str_replace;
+
 class Router
 {
 
     public array $routes = [
         'home' => ['controller' => 'FrontController', 'action' => 'homeAction', 'url' => '/home'],
-        'articlesListing' => ['controller' => 'FrontController', 'action' => 'articlesListingAction', 'url' => '/listing'],
+        'articlesListing' => ['controller' => 'FrontController', 'action' => 'articlesListingAction', 'url' => '/articles'],
         'createArticle' => ['controller' => 'AdminController', 'action' => 'createArticleAction', 'url' => '/articles/add'],
-        'detailArticle' => ['controller' => 'FrontController', 'action' => 'detailArticleAction', 'url' => '/articles/show'],
+        'detailArticle' => ['controller' => 'FrontController', 'action' => 'detailArticleAction', 'url' => '/articles/:id'],
         'register' => ['controller' => 'FrontController', 'action' => 'createUserAction', 'url' => '/register'],
         'contact' => ['controller' => 'FrontController', 'action' => 'contactAction', 'url' => '/contact'],
         'login' => ['controller' => 'FrontController', 'action' => 'loginAction', 'url' => '/login'],
@@ -24,10 +26,14 @@ class Router
     {
         $route = $this->getRouteFromUri($_SERVER['REQUEST_URI']);
         if ($route) {
-            $controllerName = 'Blog\\Controller\\'.$route['controller'];
-            $actionName = $route['action'];
+            $controllerName = 'Blog\\Controller\\'.$route['route']['controller'];
+            $actionName = $route['route']['action'];
             $controller = new $controllerName($this);
-            $controller->$actionName();
+            if($route['params']) {
+                $controller->$actionName(...$route['params']);
+            }else{
+                $controller->$actionName();
+            }
         } else {
             header('Location: /home');
             exit;
@@ -45,11 +51,21 @@ class Router
     private function getRouteFromUri(string $uri) : ?array
     {
         foreach($this->routes as $route) {
-            if ($route['url'] === $uri) {
-                return $route;
+            $regex = $this->getRegex($route['url']);
+            if(preg_match($regex, $uri, $matches)) {
+                unset($matches[0]);
+                return [
+                  'route' => $route,
+                  'params' => count($matches) > 0 ? $matches : null
+                ];
             }
         }
         return null;
+    }
+
+    private function getRegex(string $uri): string
+    {
+        return "#".str_replace(':id','([0-9]+)', $uri)."$#";
     }
 
 }
